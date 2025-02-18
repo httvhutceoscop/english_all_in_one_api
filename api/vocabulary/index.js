05b37dcea920337cfac65db310a575b15ffcd0bd
Viet NT <tienvietnguyen1110@gmail.com>
2025-02-18 15:43:17 +0700

    add api vocabulary

---
+++ api/vocabulary/index.js
@@ -0,0 +1,20 @@
+[
+    {
+        "title": "TOEIC",
+        "imageUrl": "https://www.smartcom.vn/wp-content/uploads/2024/05/toeic-smartcom-english_optimized.jpg",
+        "key": "toeic",
+    }, {
+        "title": "TOEFL",
+        "imageUrl": "https://www.smartcom.vn/wp-content/uploads/2024/05/TOEFL-la-gi.jpg",
+        "key": "toefl"
+    }, {
+        "title": "Oxford",
+        "imageUrl": "https://vi.vnp.edu.vn/wp-content/uploads/2022/05/University-of-Oxford-Acceptance-Rate.jpg",
+        "key": "oxford"
+    }
+    , {
+        "title": "IELTS",
+        "imageUrl": "https://ktdcgroup.vn/wp-content/uploads/2024/07/IELTS-la-gi-Tim-hieu-ve-ky-thi-IELTS.jpg",
+        "key": "ielts"
+    }
+]
