13d4e46c79a69803292c424c57dc1b5ff36282c2
Viet NT <tienvietnguyen1110@gmail.com>
2025-02-20 13:38:38 +0700

    add subtitle key

--- api/vocabulary/index.js
+++ api/vocabulary/index.js
@@ -1,19 +1,23 @@
 [
     {
         "title": "TOEIC",
+        "subTitle": "",
         "imageUrl": "https://www.smartcom.vn/wp-content/uploads/2024/05/toeic-smartcom-english_optimized.jpg",
         "key": "toeic"
     }, {
         "title": "TOEFL",
+        "subTitle": "",
         "imageUrl": "https://www.smartcom.vn/wp-content/uploads/2024/05/TOEFL-la-gi.jpg",
         "key": "toefl"
     }, {
         "title": "Oxford",
+        "subTitle": "",
         "imageUrl": "https://vi.vnp.edu.vn/wp-content/uploads/2022/05/University-of-Oxford-Acceptance-Rate.jpg",
         "key": "oxford"
     }
     , {
         "title": "IELTS",
+        "subTitle": "",
         "imageUrl": "https://ktdcgroup.vn/wp-content/uploads/2024/07/IELTS-la-gi-Tim-hieu-ve-ky-thi-IELTS.jpg",
         "key": "ielts"
     }
