1e979d80ee1633de4ffd250fd66c2739719f2436
Viet NT <vietnt.working@gmail.com>
2025-04-10 19:53:20 +0700

    update voc

--- api/vocabulary/index.js
+++ api/vocabulary/index.js
@@ -2,23 +2,23 @@
     {
         "title": "TOEIC",
         "subTitle": "",
-        "imageUrl": "https://www.smartcom.vn/wp-content/uploads/2024/05/toeic-smartcom-english_optimized.jpg",
-        "key": "toeic"
+        "imageUrl": "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/toeic.jpg",
+        "key": "tu-vung-toeic"
     }, {
         "title": "TOEFL",
         "subTitle": "",
-        "imageUrl": "https://www.smartcom.vn/wp-content/uploads/2024/05/TOEFL-la-gi.jpg",
-        "key": "toefl"
+        "imageUrl": "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/toefl.jpg",
+        "key": "tu-vung-toefl"
     }, {
         "title": "Oxford",
         "subTitle": "",
-        "imageUrl": "https://vi.vnp.edu.vn/wp-content/uploads/2022/05/University-of-Oxford-Acceptance-Rate.jpg",
-        "key": "oxford"
+        "imageUrl": "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/oxford.jpg",
+        "key": "tu-vung-oxford"
     }
     , {
         "title": "IELTS",
         "subTitle": "",
-        "imageUrl": "https://ktdcgroup.vn/wp-content/uploads/2024/07/IELTS-la-gi-Tim-hieu-ve-ky-thi-IELTS.jpg",
-        "key": "ielts"
+        "imageUrl": "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/ielts.jpg",
+        "key": "tu-vung-ielts"
     }
 ]
