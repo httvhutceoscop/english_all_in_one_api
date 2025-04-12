40404afe2f5a5a71149e2beda457877e1cb20788
Viet NT <tienvietnguyen1110@gmail.com>
2025-04-12 22:53:08 +0700

    Create index.js

---
+++ api/danhngon/index.js
@@ -0,0 +1,34 @@
+[
+    {
+        "title": "Công việc",
+        "subTitle": "",
+        "imageUrl": "",
+        "key": "cong-viec",
+        "totalPage": 4
+    }, {
+        "title": "Cuộc sống",
+        "subTitle": "",
+        "imageUrl": "",
+        "key": "cuoc-song",
+        "totalPage": 4
+    }, {
+        "title": "Gia đình",
+        "subTitle": "",
+        "imageUrl": "",
+        "key": "gia-dinh",
+        "totalPage": 4
+    }
+    , {
+        "title": "Tình bạn",
+        "subTitle": "",
+        "imageUrl": "",
+        "key": "tinh-ban",
+        "totalPage": 4
+    }, {
+        "title": "Tình yêu",
+        "subTitle": "",
+        "imageUrl": "",
+        "key": "tinh-yeu",
+        "totalPage": 3
+    }
+]
