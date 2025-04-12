8961d2a1458ff3d496582e626b83b1958525cf4d
Viet NT <tienvietnguyen1110@gmail.com>
2025-04-12 11:39:01 +0700

    add total page

--- api/vocabulary/index.js
+++ api/vocabulary/index.js
@@ -3,22 +3,62 @@
         "title": "TOEIC",
         "subTitle": "",
         "imageUrl": "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/toeic.jpg",
-        "key": "tu-vung-toeic"
+        "key": "tu-vung-toeic",
+        "totalPage": 3
     }, {
         "title": "TOEFL",
         "subTitle": "",
         "imageUrl": "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/toefl.jpg",
-        "key": "tu-vung-toefl"
+        "key": "tu-vung-toefl",
+        "totalPage": 1
     }, {
         "title": "Oxford",
         "subTitle": "",
         "imageUrl": "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/oxford.jpg",
-        "key": "tu-vung-oxford"
+        "key": "tu-vung-oxford",
+        "totalPage": 5
     }
     , {
         "title": "IELTS",
         "subTitle": "",
         "imageUrl": "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/ielts.jpg",
-        "key": "tu-vung-ielts"
+        "key": "tu-vung-ielts",
+        "totalPage": 1
+    }, {
+        "title": "SAT",
+        "subTitle": "",
+        "imageUrl": "",
+        "key": "tu-vung-sat",
+        "totalPage": 5
+    }, {
+        "title": "Advanced Vocabulary",
+        "subTitle": "",
+        "imageUrl": "",
+        "key": "tu-vung-cao-cap",
+        "totalPage": 1
+    }, {
+        "title": "Intermediate Vocabulary",
+        "subTitle": "",
+        "imageUrl": "",
+        "key": "tu-vung-trung-cap",
+        "totalPage": 1
+    }, {
+        "title": "Basic Vocabulary",
+        "subTitle": "",
+        "imageUrl": "",
+        "key": "tu-vung-so-cap",
+        "totalPage": 2
+    }, {
+        "title": "OFFICES",
+        "subTitle": "",
+        "imageUrl": "",
+        "key": "tu-vung-van-phong",
+        "totalPage": 1
+    }, {
+        "title": "STUDENTS",
+        "subTitle": "",
+        "imageUrl": "",
+        "key": "tu-vung-",
+        "totalPage": 1
     }
 ]
