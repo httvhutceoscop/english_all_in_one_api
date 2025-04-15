bdd20edc676a46e80fcdb10638c4b983293c3dea
Viet NT <vietnt.working@gmail.com>
2025-04-15 11:57:55 +0700

    add image for danh ngon

--- api/danhngon/index.js
+++ api/danhngon/index.js
@@ -2,32 +2,32 @@
     {
         "title": "Công việc",
         "subTitle": "",
-        "imageUrl": "",
+        "imageUrl": "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/cong-viec.jpg",
         "key": "cong-viec",
         "totalPage": 2
     }, {
         "title": "Cuộc sống",
         "subTitle": "",
-        "imageUrl": "",
+        "imageUrl": "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/cuoc-song.jpg",
         "key": "cuoc-song",
         "totalPage": 3
     }, {
         "title": "Gia đình",
         "subTitle": "",
-        "imageUrl": "",
+        "imageUrl": "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/gia-dinh.jpg",
         "key": "gia-dinh",
         "totalPage": 3
     }
     , {
         "title": "Tình bạn",
         "subTitle": "",
-        "imageUrl": "",
+        "imageUrl": "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/tinh-ban.jpg",
         "key": "tinh-ban",
         "totalPage": 2
     }, {
         "title": "Tình yêu",
         "subTitle": "",
-        "imageUrl": "",
+        "imageUrl": "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/tinh-yeu.jpg",
         "key": "tinh-yeu",
         "totalPage": 3
     }
