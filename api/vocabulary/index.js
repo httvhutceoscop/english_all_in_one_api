fee77133715a8a4bfab311299591be16eeeca280
Viet NT <vietnt.working@gmail.com>
2025-04-24 14:11:26 +0700

    update list cat vocab

--- api/vocabulary/index.js
+++ api/vocabulary/index.js
@@ -1,64 +1,90 @@
 [
-    {
-        "title": "TOEIC",
-        "subTitle": "",
-        "imageUrl": "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/toeic.jpg",
-        "key": "tu-vung-toeic",
-        "totalPage": 3
-    }, {
-        "title": "TOEFL",
-        "subTitle": "",
-        "imageUrl": "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/toefl.jpg",
-        "key": "tu-vung-toefl",
-        "totalPage": 1
-    }, {
-        "title": "Oxford",
-        "subTitle": "",
-        "imageUrl": "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/oxford.jpg",
-        "key": "tu-vung-oxford",
-        "totalPage": 5
-    }
-    , {
-        "title": "IELTS",
-        "subTitle": "",
-        "imageUrl": "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/ielts.jpg",
-        "key": "tu-vung-ielts",
-        "totalPage": 1
-    }, {
-        "title": "SAT",
-        "subTitle": "",
-        "imageUrl": "",
-        "key": "tu-vung-sat",
-        "totalPage": 5
-    }, {
-        "title": "Advanced Vocabulary",
-        "subTitle": "",
-        "imageUrl": "",
-        "key": "tu-vung-cao-cap",
-        "totalPage": 1
-    }, {
-        "title": "Intermediate Vocabulary",
-        "subTitle": "",
-        "imageUrl": "",
-        "key": "tu-vung-trung-cap",
-        "totalPage": 1
-    }, {
-        "title": "Basic Vocabulary",
-        "subTitle": "",
-        "imageUrl": "",
-        "key": "tu-vung-so-cap",
-        "totalPage": 2
-    }, {
-        "title": "OFFICES",
-        "subTitle": "",
-        "imageUrl": "",
-        "key": "tu-vung-van-phong",
-        "totalPage": 1
-    }, {
-        "title": "STUDENTS",
-        "subTitle": "",
-        "imageUrl": "",
-        "key": "tu-vung-",
-        "totalPage": 1
-    }
-]
+  {
+    title: "TOEIC",
+    subTitle: "",
+    imageUrl:
+      "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/toeic.jpg",
+    key: "tu-vung-toeic",
+    totalPage: 3,
+  },
+  {
+    title: "TOEFL",
+    subTitle: "",
+    imageUrl:
+      "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/toefl.jpg",
+    key: "tu-vung-toefl",
+    totalPage: 1,
+  },
+  {
+    title: "Oxford",
+    subTitle: "",
+    imageUrl:
+      "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/oxford.jpg",
+    key: "tu-vung-oxford",
+    totalPage: 5,
+  },
+  {
+    title: "IELTS",
+    subTitle: "",
+    imageUrl:
+      "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/ielts.jpg",
+    key: "tu-vung-ielts",
+    totalPage: 1,
+  },
+  {
+    title: "SAT",
+    subTitle: "",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/SAT_logo_%282017%29.svg/500px-SAT_logo_%282017%29.svg.png",
+    key: "tu-vung-sat",
+    totalPage: 5,
+  },
+  {
+    title: "Advanced Vocabulary",
+    subTitle: "",
+    imageUrl:
+      "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/advanced-vocabulary.jpg",
+    key: "tu-vung-cao-cap",
+    totalPage: 1,
+  },
+  {
+    title: "Intermediate Vocabulary",
+    subTitle: "",
+    imageUrl:
+      "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/intermediate-vocabulary.jpg",
+    key: "tu-vung-trung-cap",
+    totalPage: 1,
+  },
+  {
+    title: "Basic Vocabulary",
+    subTitle: "",
+    imageUrl:
+      "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/basic-vocabulary.jpg",
+    key: "tu-vung-so-cap",
+    totalPage: 2,
+  },
+  {
+    title: "OFFICES",
+    subTitle: "",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Channel_1_Israel_DSC0021.jpg/960px-Channel_1_Israel_DSC0021.jpg",
+    key: "tu-vung-van-phong",
+    totalPage: 1,
+  },
+  {
+    title: "STUDENTS",
+    subTitle: "",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/High_school_students%2C_SAS_Pudong.jpg/960px-High_school_students%2C_SAS_Pudong.jpg",
+    key: "tu-vung-",
+    totalPage: 1,
+  },
+  {
+    title: "Flowers",
+    subTitle: "",
+    imageUrl:
+      "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/flowers.jpg",
+    key: "tu-vung-",
+    totalPage: 1,
+  },
+];
