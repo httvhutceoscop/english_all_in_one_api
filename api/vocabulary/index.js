6e10868eace864904de1781b47cc30ab19688c24
Viet NT <vietnt.working@gmail.com>
2025-04-29 14:21:44 +0700

    validate json

--- api/vocabulary/index.js
+++ api/vocabulary/index.js
@@ -1,106 +1,106 @@
 [
   {
-    title: "TOEIC",
-    subTitle: "",
-    imageUrl:
+    "title": "TOEIC",
+    "subTitle": "",
+    "imageUrl":
       "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/toeic.jpg",
-    key: "tu-vung-toeic",
-    totalPage: 3,
+    "key": "tu-vung-toeic",
+    "totalPage": 3
   },
   {
-    title: "TOEFL",
-    subTitle: "",
-    imageUrl:
+    "title": "TOEFL",
+    "subTitle": "",
+    "imageUrl":
       "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/toefl.jpg",
-    key: "tu-vung-toefl",
-    totalPage: 1,
+    "key": "tu-vung-toefl",
+    "totalPage": 1
   },
   {
-    title: "Oxford",
-    subTitle: "",
-    imageUrl:
+    "title": "Oxford",
+    "subTitle": "",
+    "imageUrl":
       "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/oxford.jpg",
-    key: "tu-vung-oxford",
-    totalPage: 5,
+    "key": "tu-vung-oxford",
+    "totalPage": 5
   },
   {
-    title: "IELTS",
-    subTitle: "",
-    imageUrl:
+    "title": "IELTS",
+    "subTitle": "",
+    "imageUrl":
       "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/ielts.jpg",
-    key: "tu-vung-ielts",
-    totalPage: 1,
+    "key": "tu-vung-ielts",
+    "totalPage": 1
   },
   {
-    title: "SAT",
-    subTitle: "",
-    imageUrl:
+    "title": "SAT",
+    "subTitle": "",
+    "imageUrl":
       "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/SAT_logo_%282017%29.svg/500px-SAT_logo_%282017%29.svg.png",
-    key: "tu-vung-sat",
-    totalPage: 5,
+    "key": "tu-vung-sat",
+    "totalPage": 5
   },
   {
-    title: "Advanced Vocabulary",
-    subTitle: "",
-    imageUrl:
+    "title": "Advanced Vocabulary",
+    "subTitle": "",
+    "imageUrl":
       "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/advanced-vocabulary.jpg",
-    key: "tu-vung-cao-cap",
-    totalPage: 1,
+    "key": "tu-vung-cao-cap",
+    "totalPage": 1
   },
   {
-    title: "Intermediate Vocabulary",
-    subTitle: "",
-    imageUrl:
+    "title": "Intermediate Vocabulary",
+    "subTitle": "",
+    "imageUrl":
       "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/intermediate-vocabulary.jpg",
-    key: "tu-vung-trung-cap",
-    totalPage: 1,
+    "key": "tu-vung-trung-cap",
+    "totalPage": 1
   },
   {
-    title: "Basic Vocabulary",
-    subTitle: "",
-    imageUrl:
+    "title": "Basic Vocabulary",
+    "subTitle": "",
+    "imageUrl":
       "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/basic-vocabulary.jpg",
-    key: "tu-vung-so-cap",
-    totalPage: 2,
+    "key": "tu-vung-so-cap",
+    "totalPage": 2
   },
   {
-    title: "Adventure Phrases",
-    subTitle: "",
-    imageUrl:
+    "title": "Adventure Phrases",
+    "subTitle": "",
+    "imageUrl":
       "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/english-adventure-phrases.jpg",
-    key: "english-adventure-phrases",
-    totalPage: 2,
+    "key": "english-adventure-phrases",
+    "totalPage": 2
   },
   {
-    title: "Adventure Words",
-    subTitle: "",
-    imageUrl:
+    "title": "Adventure Words",
+    "subTitle": "",
+    "imageUrl":
       "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/english-adventure-words.jpg",
-    key: "english-adventure-words",
-    totalPage: 6,
+    "key": "english-adventure-words",
+    "totalPage": 6
   },
   {
-    title: "OFFICES",
-    subTitle: "",
-    imageUrl:
+    "title": "OFFICES",
+    "subTitle": "",
+    "imageUrl":
       "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/office.jpg",
-    key: "tu-vung-van-phong",
-    totalPage: 1,
+    "key": "tu-vung-van-phong",
+    "totalPage": 1
   },
   {
-    title: "STUDENTS",
-    subTitle: "",
-    imageUrl:
+    "title": "STUDENTS",
+    "subTitle": "",
+    "imageUrl":
       "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/students.jpg",
-    key: "tu-vung-",
-    totalPage: 1,
+    "key": "tu-vung-",
+    "totalPage": 1
   },
   {
-    title: "Flowers",
-    subTitle: "",
-    imageUrl:
+    "title": "Flowers",
+    "subTitle": "",
+    "imageUrl":
       "https://httvhutceoscop.github.io/english_all_in_one_api/assets/img/flowers.jpg",
-    key: "tu-vung-",
-    totalPage: 1,
-  },
+    "key": "tu-vung-",
+    "totalPage": 1
+  }
 ]
